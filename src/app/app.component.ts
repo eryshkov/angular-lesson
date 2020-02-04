7a747c9ccda0fffe62c028071f5286c0db7b7415
Evgenii Ryshkov <evgeny.ryshkov@kassa.com>
2020-02-04 19:05:34 +0300

    добавлен todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  date?: any;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   appTitle = 'Angular application';
+
+  public todos: Todo[] = [
+    {id: 1, title: 'Купить хлеб', completed: false, date: new Date()},
+    {id: 2, title: 'Купить икру', completed: true, date: new Date()},
+    {id: 3, title: 'Купить орехи', completed: false, date: new Date()},
+  ];
 }
